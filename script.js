8e3492f58ed3d7858f0dad536e5dc901c58f5c88
Gabriel Franco <95918152+GabrielFranco01@users.noreply.github.com>
2022-03-30 17:27:41 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function entrar(){
+    var a = document.getElementsByClassName('areaservico')
+    a.style.color = 'blue'
+} function sair (){
+    var a = document.getElementsByClassName ('areaservico')
+    a.style.color = 'black'
+} 
